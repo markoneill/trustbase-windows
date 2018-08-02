9f688d5e07e3825e063dfb01e9fef18e1134f0c1
Dallas Williamson <dallas.v.williamson@gmail.com>
2018-08-01 19:30:01 -0600

    This could potentially help
    Sorry, if this works, then computer crashes, so I hav to be sure to save on git first

--- TrustBaseInstaller/TrustBaseBin/runadmin.bat
+++ TrustBaseInstaller/TrustBaseBin/runadmin.bat
@@ -7,6 +7,10 @@ set python=HKEY_CURRENT_USER\SOFTWARE\Python\PythonCore\2.7\
 set vsdist=HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Installer\Dependencies\Microsoft.VS.VC_RuntimeAdditionalVSU_amd64,v14\
 set err=0
 
+:: Get the short name of the base_dir
+set short_name=
+for %%I in (.) do set short_name=%%~sI /a
+
 :preinstalls
 REG QUERY %python% 1>nul 2>nul || set /a err=err+1
 REG QUERY %vsdist% 1>nul 2>nul || set /a err=err+2
@@ -32,20 +36,6 @@ powershell -Command "Start-Process .\preinstall.exe -Verb RunAs -Wait" 1>nul 2>n
 cd %pwd%
 
 :starttrustbase
-
-set init=0
-for %%I in (.) do (
-    if %init% EQU 0 (
-        set short_name=%%~sI
-    	set init=1
-    ) 
-
-    if %init% NEQ 0 (
-        set short_name=%%~sI /a
-    )
-)
-
-rem RUNDLL32.EXE SETUPAPI.DLL,InstallHinfSection DefaultInstall 132 .\release\TrustBaseWin\TrustBaseWin.inf
 powershell -Command "Start-Process cmd -Verb RunAs -ArgumentList '/C cd %short_name% && RUNDLL32.EXE SETUPAPI.DLL,InstallHinfSection DefaultInstall 132 .\release\TrustBaseWin\TrustBaseWin.inf'"
 powershell -Command "Start-Process cmd -Verb RunAs -ArgumentList '/c net start TrustBaseWin'"
 powershell -Command "Start-Process cmd -Verb RunAs -ArgumentList '/k cd %cwd% && PolicyEngine.exe'"
